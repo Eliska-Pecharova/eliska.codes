509c0f6f209015f153e75fa991756b5ecb54b60b
evonox <prehnal@gmail.com>
2026-02-16 23:35:11 +0100

    Kritika main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+/**
+ * Doporucuji provést kontrolu, že ten #root element opravdu existuje, jinak chyba do console.error(...)
+ */
+const root = document.getElementById("root");
+if(root !== null) {
+    // Doporuceno vkladat StrictMode element, odhalí se tím ve fázi ladění mnoho chyb
+    createRoot(root).render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+} else {
+    console.error("Element #root nebyl nalezen.");
+}
